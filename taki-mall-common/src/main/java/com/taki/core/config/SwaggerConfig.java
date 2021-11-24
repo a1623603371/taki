490cb92e97cb5faa1736ef4de1e03afef82a918b
long <17573404517@163.com>
2021-11-24 23:48:42 +0800

    1.增加全局统一异常 2.增加统一数据返回格式 3.配置swagger

---
+++ taki-mall-common/src/main/java/com/taki/core/config/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package com.taki.core.config;
+
+import io.swagger.annotations.ApiOperation;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * @ClassName SwaggerConfig
+ * @Description swagger 配置
+ * @Author Long
+ * @Date 2021/11/23 22:44
+ * @Version 1.0
+ */
+@Configuration
+@EnableOpenApi
+public class SwaggerConfig {
+
+     @Bean
+     public Docket createRestApi(){
+
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("taki 商城 API 文档")
+                .description("测试接口文档")
+                .contact(new Contact("Long", "作者网站(url)", "1623603371@qq.com(email)"))
+                .version("1.0")
+                .build();
+    }
+
+
+
+}
