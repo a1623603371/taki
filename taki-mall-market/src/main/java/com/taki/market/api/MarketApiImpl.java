75df490d33afd31f0f7e0d1b51c894591d4cbb97
long <17573404517@163.com>
2022-02-26 17:38:56 +0800

    营销服务 （版本1.0）api 接口 实现

--- taki-mall-market/src/main/java/com/taki/market/api/MarketApiImpl.java
+++ taki-mall-market/src/main/java/com/taki/market/api/MarketApiImpl.java
@@ -8,39 +8,81 @@ import com.taki.market.request.CancelOrderReleaseUserCouponRequest;
 import com.taki.market.request.LockUserCouponRequest;
 import com.taki.market.domain.query.UserCouponQuery;
 import com.taki.market.request.ReleaseUserCouponRequest;
+import com.taki.market.service.CouponService;
+import com.taki.market.service.MarketService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @ClassName MarketApiImpl
- * @Description TODO
+ * @Description 营销系统API
  * @Author Long
  * @Date 2022/2/18 11:49
  * @Version 1.0
  */
 @DubboService(version = "1.0.0",interfaceClass = MarketApi.class,retries = 0)
+@Slf4j
 public class MarketApiImpl implements MarketApi{
+
+
+    @Autowired
+    private CouponService couponService;
+
+    @Autowired
+    private MarketService marketService;
+
+
     @Override
     public ResponseData<CalculateOrderAmountDTO> calculateOrderAmount(CalculateOrderAmountRequest calculateOrderAmountRequest) {
-        return null;
+
+        try {
+            CalculateOrderAmountDTO calculateOrderAmountDTO =   marketService.calculateOrderAmount(calculateOrderAmountRequest);
+            return ResponseData.success(calculateOrderAmountDTO);
+        }catch (Exception e){
+            log.error("biz error",e);
+            return ResponseData.error(e.getMessage());
+        }
     }
 
     @Override
     public ResponseData<Boolean> lockUserCoupon(LockUserCouponRequest lockUserCouponRequest) {
-        return null;
+        try {
+            Boolean result =   couponService.lockUserCoupon(lockUserCouponRequest);
+            return ResponseData.success(result);
+        }catch (Exception e){
+            log.error("biz error",e);
+            return ResponseData.error(e.getMessage());
+        }
     }
 
     @Override
     public ResponseData<Boolean> releaseUserCoupon(ReleaseUserCouponRequest releaseUserCouponRequest) {
-        return null;
+        try {
+            Boolean result =   couponService.releaseUserCoupon(releaseUserCouponRequest);
+            return ResponseData.success(result);
+        }catch (Exception e){
+            log.error("biz error",e);
+            return ResponseData.error(e.getMessage());
+        }
     }
 
     @Override
     public ResponseData<UserCouponDTO> queryUserCoupon(UserCouponQuery userCouponQuery) {
-        return null;
+        try {
+            UserCouponDTO userCouponDTO = couponService.getUserCoupon(userCouponQuery);
+            return ResponseData.success(userCouponDTO);
+        }catch (Exception e){
+            log.error("biz error",e);
+            return ResponseData.error(e.getMessage());
+        }
+
+
     }
 
     @Override
     public ResponseData<Boolean> cancelOrderReleaseCoupon(CancelOrderReleaseUserCouponRequest cancelOrderReleaseUserCouponRequest) {
-        return null;
+        log.info("回退优惠券,orderId:{}",cancelOrderReleaseUserCouponRequest.getOrderId());
+         return ResponseData.success(true);
     }
 }
